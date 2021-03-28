e420c2a30cf7207558a36dfcb98a0d70cd4f40ed
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2021-03-28 19:44:12 +0300

    created train table

--- filmdb/models.py
+++ filmdb/models.py
@@ -22,8 +22,6 @@ class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     gender = models.CharField(max_length=1)  # M or F
     age = models.IntegerField
-    occupation = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
     password = models.CharField(max_length=100)
 
     class Meta:
@@ -48,4 +46,15 @@ class Rating(models.Model):
 
     class Meta:
         db_table = 'Rating'
+        unique_together = (('movie_id', 'user_id'),)
+
+
+class TrainData(models.Model):
+    rating_id = models.AutoField(primary_key=True)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE, db_column='movie_id')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    rating = models.FloatField
+
+    class Meta:
+        db_table = 'TrainData'
         unique_together = (('movie_id', 'user_id'),)
